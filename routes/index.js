e4c74b34e278b46afc769ac6ccf66c33b3fca457
Matt Stuefen <mattstuefen@gmail.com>
2016-07-27 15:11:54 -0500

    Added blank navbar and modal login.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var url = require("url");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var parameters = url.parse(req.url, true).query;
+    res.render('index', { name: parameters.name });
 });
 
 module.exports = router;
